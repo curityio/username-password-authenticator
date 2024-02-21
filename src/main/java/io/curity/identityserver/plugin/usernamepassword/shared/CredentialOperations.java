e2ec1cb6caf188139403abc4c09c0e87c0ecd8ec
gary.archer <gary.archer@WORKMAC.local>
2024-02-21 13:34:14 +0100

    Updates

--- src/main/java/io/curity/identityserver/plugin/usernamepassword/shared/CredentialOperations.java
+++ src/main/java/io/curity/identityserver/plugin/usernamepassword/shared/CredentialOperations.java
@@ -1,6 +1,7 @@
 package io.curity.identityserver.plugin.usernamepassword.shared;
 
 import se.curity.identityserver.sdk.service.credential.CredentialOperationDetails;
+import se.curity.identityserver.sdk.service.credential.results.PasswordRejectedByDataSource;
 import se.curity.identityserver.sdk.service.credential.results.SubjectCredentialsNotFound;
 import se.curity.identityserver.sdk.web.Response;
 
@@ -10,7 +11,9 @@ public final class CredentialOperations {
 
     public static void onCredentialUpdateRejected(Response response, List<? extends CredentialOperationDetails.Rejected> details) {
 
-        var filteredDetails = details.stream().filter(detail -> !(detail instanceof SubjectCredentialsNotFound)).toList();
+        var filteredDetails = details.stream().filter(detail ->
+                !(detail instanceof SubjectCredentialsNotFound || detail instanceof PasswordRejectedByDataSource)
+        ).toList();
         response.putViewData("_rejection_details", filteredDetails, Response.ResponseModelScope.FAILURE);
     }
 }
